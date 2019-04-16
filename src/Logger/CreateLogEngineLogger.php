c41b74d9303ef3b9120478d8913c3a9845cbbfe7
Valerio <ilvalerione@gmail.com>
2019-04-16 18:35:27 +0200

    use log engine service instance in monolog handler factory

--- src/Logger/CreateLogEngineLogger.php
+++ src/Logger/CreateLogEngineLogger.php
@@ -3,6 +3,7 @@
 namespace LogEngine\Laravel\Logger;
 
 
+use Illuminate\Contracts\Container\Container;
 use LogEngine\LogEngine;
 use Monolog\Handler\PsrHandler;
 use Monolog\Logger;
@@ -10,19 +11,30 @@ use Monolog\Logger;
 class CreateLogEngineLogger
 {
     /**
+     * @var Container
+     */
+    public $app;
+
+    /**
+     * CreateLogEngineLogger constructor.
+     *
+     * @param Container $app
+     */
+    public function __construct(Container $app)
+    {
+        $this->app = $app;
+    }
+
+    /**
      * Create a custom Monolog instance.
      *
      * @param  array $config
      * @return \Monolog\Logger
-     * @throws \LogEngine\Exceptions\LogEngineException
      */
     public function __invoke(array $config)
     {
         return new Logger(getenv('APP_NAME'), [
-            new PsrHandler(
-                new LogEngine($config['url'], $config['key']),
-                $config['level']
-            )
+            new PsrHandler($this->app->logengine)
         ]);
     }
 }
