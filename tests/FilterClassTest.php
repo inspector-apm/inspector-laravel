cc76326f6e1937a72a3fd7a564ca6f898c44bebf
Valerio <ilvalerione@gmail.com>
2019-07-15 13:02:33 +0200

    test response data collection

--- tests/FilterClassTest.php
+++ tests/FilterClassTest.php
@@ -14,7 +14,8 @@ class FilterClassTest extends BasicTestCase
     public function testRequestApproved()
     {
         $this->app->router->get('test', function (Request $request) {
-            $this->assertTrue(Filters::isApprovedRequest($request) && Inspector::isRecording());
+            $this->assertTrue(Filters::isApprovedRequest($request));
+            $this->assertTrue(Inspector::isRecording());
         })->middleware(WebRequestMonitoring::class);
 
         $this->call('GET', 'test');
@@ -23,7 +24,8 @@ class FilterClassTest extends BasicTestCase
     public function testRequestNotApproved()
     {
         $this->app->router->get('nova', function (Request $request) {
-            $this->assertFalse(Filters::isApprovedRequest($request) || Inspector::isRecording());
+            $this->assertFalse(Filters::isApprovedRequest($request));
+            $this->assertFalse(Inspector::isRecording());
         })->middleware(WebRequestMonitoring::class);
 
         $this->call('GET', 'nova');
