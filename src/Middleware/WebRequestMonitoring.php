0d1447abb4363ba225857b543fb0f4a491d4ab21
Valerio <ilvalerione@gmail.com>
2019-07-17 10:34:40 +0200

    fix with user

--- src/Middleware/WebRequestMonitoring.php
+++ src/Middleware/WebRequestMonitoring.php
@@ -44,10 +44,7 @@ class WebRequestMonitoring implements TerminableInterface
         );
 
         if (Auth::check() && config('inspector.user')) {
-            $transaction->withUser(
-                Auth::user()->getAuthIdentifier(),
-                Auth::user()->getAuthIdentifierName()
-            );
+            $transaction->withUser(Auth::user()->getAuthIdentifier());
         }
     }
 
