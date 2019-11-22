38c22bb4aac0bfece43c90be5135697d8e019f85
Valerio Barbera <ilvalerione@gmail.com>
2019-11-22 11:36:10 +0100

    passing not allowed collections to filters methods

--- tests/FilterClassTest.php
+++ tests/FilterClassTest.php
@@ -14,7 +14,7 @@ class FilterClassTest extends BasicTestCase
     public function testRequestApproved()
     {
         $this->app->router->get('test', function (Request $request) {
-            $this->assertTrue(Filters::isApprovedRequest($request));
+            $this->assertTrue(Filters::isApprovedRequest(config('inspector.ignore_url'), $request));
         })->middleware(WebRequestMonitoring::class);
 
         $this->call('GET', 'test');
@@ -23,7 +23,7 @@ class FilterClassTest extends BasicTestCase
     public function testRequestNotApproved()
     {
         $this->app->router->get('nova', function (Request $request) {
-            $this->assertFalse(Filters::isApprovedRequest($request));
+            $this->assertFalse(Filters::isApprovedRequest(config('inspector.ignore_url'), $request));
         })->middleware(WebRequestMonitoring::class);
 
         $this->call('GET', 'nova');
