0224fd05ee4ffc95a117913bdbd1b80d16f22c88
Valerio Barbera <ilvalerione@gmail.com>
2020-09-16 14:54:35 +0200

    laravel 8 support

--- src/InspectorServiceProvider.php
+++ src/InspectorServiceProvider.php
@@ -23,7 +23,7 @@ class InspectorServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '4.4.19';
+    const VERSION = '4.5.0';
 
     /**
      * Booting of services.
