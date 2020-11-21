b2b5d63fb0ccde76afca5d083554b7f29ea05ed9
Valerio Barbera <ilvalerione@gmail.com>
2020-11-21 12:40:12 +0100

    refactor

--- src/InspectorServiceProvider.php
+++ src/InspectorServiceProvider.php
@@ -23,7 +23,7 @@ class InspectorServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '4.6.1';
+    const VERSION = '4.6.2';
 
     /**
      * Booting of services.
