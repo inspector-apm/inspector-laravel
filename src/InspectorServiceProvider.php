6f8121014186c5ac2b165e2500bf909533a147bb
Valerio Barbera <ilvalerione@gmail.com>
2020-11-27 16:23:13 +0100

    new version

--- src/InspectorServiceProvider.php
+++ src/InspectorServiceProvider.php
@@ -23,7 +23,7 @@ class InspectorServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '4.6.2';
+    const VERSION = '4.6.3';
 
     /**
      * Booting of services.
