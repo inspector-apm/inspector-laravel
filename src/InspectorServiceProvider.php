3e86f31b45e63d769045e48660ddd353011c0a1d
Valerio Barbera <ilvalerione@gmail.com>
2020-11-30 15:58:44 +0100

    change email segment id tracking

--- src/InspectorServiceProvider.php
+++ src/InspectorServiceProvider.php
@@ -23,7 +23,7 @@ class InspectorServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '4.6.4';
+    const VERSION = '4.6.5';
 
     /**
      * Booting of services.
