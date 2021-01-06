683f1ef4481000ca18ad20f7e31c7038dcdfb00d
Valerio Barbera <ilvalerione@gmail.com>
2021-01-06 10:25:48 +0100

    fix logs report

--- src/InspectorServiceProvider.php
+++ src/InspectorServiceProvider.php
@@ -23,7 +23,7 @@ class InspectorServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '4.6.10';
+    const VERSION = '4.6.11';
 
     /**
      * Booting of services.
