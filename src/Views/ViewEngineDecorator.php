2963149f4068f1ac0126dba28351c2f831a7ee7d
Valerio Barbera <ilvalerione@gmail.com>
2021-02-05 19:58:49 +0100

    view engine decorator

--- src/Views/ViewEngineDecorator.php
+++ src/Views/ViewEngineDecorator.php
@@ -41,7 +41,7 @@ final class ViewEngineDecorator implements Engine
         $label = 'view:'.$this->viewFactory->shared(self::SHARED_KEY, basename($path));
 
         return Inspector::addSegment(function (Segment $segment) use ($path, $data) {
-            $segment->addContext('info', compact('path', 'data'));
+            $segment->addContext('details', compact('path', 'data'));
 
             return $this->engine->get($path, $data);
         }, 'view.render', $label);
