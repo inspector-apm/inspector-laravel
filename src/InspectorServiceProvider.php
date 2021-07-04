2513417b67356103b47f03ad19f0a3984880a7af
Valerio Barbera <ilvalerione@gmail.com>
2021-07-04 18:30:43 +0200

    out of memory bootstrapper

--- src/InspectorServiceProvider.php
+++ src/InspectorServiceProvider.php
@@ -30,7 +30,7 @@ class InspectorServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '4.7.5';
+    const VERSION = '4.7.6';
 
     /**
      * Booting of services.
