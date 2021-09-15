7b06198b12afce67dbcd43321c790e3317222b78
Valerio Barbera <ilvalerione@gmail.com>
2021-09-15 13:17:55 +0200

    fix failed jobs reporting

--- src/InspectorServiceProvider.php
+++ src/InspectorServiceProvider.php
@@ -30,7 +30,7 @@ class InspectorServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '4.7.13';
+    const VERSION = '4.7.14';
 
     /**
      * Booting of services.
