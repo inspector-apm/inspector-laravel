cd2a9d44a19366e87cd5b1d90d983dc2df609a13
Valerio Barbera <ilvalerione@gmail.com>
2022-02-09 15:07:20 +0100

    update version

--- src/InspectorServiceProvider.php
+++ src/InspectorServiceProvider.php
@@ -30,7 +30,7 @@ class InspectorServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '4.7.17';
+    const VERSION = '4.7.18';
 
     /**
      * Booting of services.
