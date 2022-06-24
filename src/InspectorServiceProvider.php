5ff060b8594e37af260ea7cc373723db26c698c4
Valerio Barbera <ilvalerione@gmail.com>
2022-06-24 19:21:50 +0200

    fix config instance

--- src/InspectorServiceProvider.php
+++ src/InspectorServiceProvider.php
@@ -30,7 +30,7 @@ class InspectorServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '4.7.23';
+    const VERSION = '4.7.24';
 
     /**
      * Booting of services.
