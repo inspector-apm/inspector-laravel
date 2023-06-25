9b8f8697c8926966dd9be65c271734d541673c6d
Valerio Barbera <ilvalerione@gmail.com>
2023-06-25 19:47:55 +0200

    extend segment label

--- src/InspectorServiceProvider.php
+++ src/InspectorServiceProvider.php
@@ -29,7 +29,7 @@ class InspectorServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '4.7.30';
+    const VERSION = '4.7.31';
 
     /**
      * Booting of services.
