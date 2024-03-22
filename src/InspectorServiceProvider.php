4323010221187ff8aee3280fbe5757ba95b6cb31
Valerio Barbera <ilvalerione@gmail.com>
2024-03-22 17:11:07 +0100

    New middleware for Laravel Octane

--- src/InspectorServiceProvider.php
+++ src/InspectorServiceProvider.php
@@ -29,7 +29,7 @@ class InspectorServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '4.8.0';
+    const VERSION = '4.8.1';
 
     /**
      * Booting of services.
