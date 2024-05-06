c280e7371adc925dd85c76f20dc8adf865aa6fe0
Valerio Barbera <ilvalerione@gmail.com>
2024-05-06 12:53:40 +0200

    fix job monitoring for dispatch_sync

--- src/InspectorServiceProvider.php
+++ src/InspectorServiceProvider.php
@@ -29,7 +29,7 @@ class InspectorServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '4.8.3';
+    const VERSION = '4.8.4';
 
     /**
      * Booting of services.
