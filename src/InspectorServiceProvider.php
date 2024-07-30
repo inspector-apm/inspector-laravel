a4bc030e63bee6f6fb4361b2e96fb1471b9989e2
Valerio Barbera <ilvalerione@gmail.com>
2024-07-30 12:03:41 +0200

    response body

--- src/InspectorServiceProvider.php
+++ src/InspectorServiceProvider.php
@@ -29,7 +29,7 @@ class InspectorServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '4.9.7';
+    const VERSION = '4.9.8';
 
     /**
      * Booting of services.
