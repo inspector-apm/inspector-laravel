a9ebd11750bc9315f4a614b2808e529b6ad02ab0
Valerio Barbera <ilvalerione@gmail.com>
2024-10-15 09:53:09 +0200

    collect headers on Laravel Octane

--- src/InspectorServiceProvider.php
+++ src/InspectorServiceProvider.php
@@ -29,7 +29,7 @@ class InspectorServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '4.9.13';
+    const VERSION = '4.9.14';
 
     /**
      * Booting of services.
