ed9a5d03c487f7a7e76892e1ba8149fe8ba78cd8
Valerio Barbera <ilvalerione@gmail.com>
2024-10-23 17:19:06 +0200

    fix missing query sql

--- src/InspectorServiceProvider.php
+++ src/InspectorServiceProvider.php
@@ -29,7 +29,7 @@ class InspectorServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '4.9.14';
+    const VERSION = '4.9.15';
 
     /**
      * Booting of services.
