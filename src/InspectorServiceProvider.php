b99d6e27a263c7b27250430d83ad922b9d59b797
Valerio Barbera <ilvalerione@gmail.com>
2025-01-19 16:08:49 +0100

    use wildcard to default ignore commands

--- src/InspectorServiceProvider.php
+++ src/InspectorServiceProvider.php
@@ -29,7 +29,7 @@ class InspectorServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '4.10.0';
+    const VERSION = '4.10.1';
 
     /**
      * Booting of services.
