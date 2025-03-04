099f41f2dde8fad771731ca1f894123b48fef210
Valerio Barbera <ilvalerione@gmail.com>
2025-03-04 11:58:13 +0100

    Neuron AI component

--- src/InspectorServiceProvider.php
+++ src/InspectorServiceProvider.php
@@ -29,7 +29,7 @@ class InspectorServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '4.11.1';
+    const VERSION = '4.11.2';
 
     /**
      * Booting of services.
