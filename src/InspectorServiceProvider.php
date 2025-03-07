773e7bc25a7735818b2ed639a0c1b0305cab3f99
Valerio Barbera <ilvalerione@gmail.com>
2025-03-07 12:10:42 +0100

    remove Neuron AI component

--- src/InspectorServiceProvider.php
+++ src/InspectorServiceProvider.php
@@ -29,7 +29,7 @@ class InspectorServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '4.11.2';
+    const VERSION = '4.11.3';
 
     /**
      * Booting of services.
