8c88a9c560111f28fd6e7fd4eb40c5d29044024f
Valerio Barbera <valerio@inspector.dev>
2025-07-09 17:27:00 +0200

    type hint

--- src/InspectorServiceProvider.php
+++ src/InspectorServiceProvider.php
@@ -29,7 +29,7 @@ class InspectorServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '4.15.0';
+    const VERSION = '4.15.1';
 
     /**
      * Booting of services.
