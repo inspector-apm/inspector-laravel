763e44f5ecd0902a90dd3d256f8607872ebce660
Valerio Barbera <valerio@inspector.dev>
2026-05-07 12:58:46 +0200

    fix mcp server monitoring

--- src/InspectorServiceProvider.php
+++ src/InspectorServiceProvider.php
@@ -34,7 +34,7 @@ class InspectorServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const VERSION = '4.18.13';
+    public const VERSION = '4.18.14';
 
     /**
      * Booting of services.
